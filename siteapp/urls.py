26f4f667355416e30e58e94a96445a7290481b70
Anurag Patnaik <anurag.patnaik@gmail.com>
2014-05-13 09:32:42 -0400

    Added blog pages. Work in progress

--- siteapp/urls.py
+++ siteapp/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from siteapp import views
 
 urlpatterns = patterns('',
+    url(r'^blog/$', views.blog, name='blog'),
     url(r'^$', views.index, name='index')
 )
--- siteapp/views.py
+++ siteapp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     context = {'message': 'Hello'}
     return render(request, 'siteapp/index.html', context)
+
+def blog(request):
+    context = {}
+    return render(request, 'siteapp/blog/index.html', context)
